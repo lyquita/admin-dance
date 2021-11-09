7e990fb1e7cde48e06797b7eac8755c16632d743
hireoochen@gmail.com <hireoochen@gmail.com>
2021-11-09 21:25:11 +0800

    finished the amount statistics ui

--- admin-dance/src/components/dashboard/AmountStatistic.tsx
+++ admin-dance/src/components/dashboard/AmountStatistic.tsx
@@ -1,10 +1,62 @@
+import { Box, Grid, Typography } from '@mui/material';
 import React from 'react';
+import { makeStyles } from '@mui/styles';
 
-const AmountStatistic =() =>{
+const useStyles = makeStyles({
+  box: {
+    background: 'white',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    borderRadius: '4px',
+    boxShadow:
+      'rgb(63 63 68 / 5%) 0px 0px 0px 1px, rgb(63 63 68 / 15%) 0px 1px 2px 0px',
+    transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
+    padding: '30px 30px',
+  },
+});
 
-    return (
-        <>
-        </>
-    );
+const AmountStatistic = () => {
+  const classes = useStyles();
 
+  return (
+    <>
+      <Grid container spacing={{ xs: 1, sm:2 }}>
+        <Grid item xs={12} sm={6}>
+          <Box className={classes.box}>
+            <Typography variant='h4'>Order Amount</Typography>
+            <Typography variant='h2' fontWeight='700'>
+              33
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box className={classes.box}>
+            <Typography variant='h4'>Order Amount</Typography>
+            <Typography variant='h2' fontWeight='700'>
+              33
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box className={classes.box}>
+            <Typography variant='h4'>Order Amount</Typography>
+            <Typography variant='h2' fontWeight='700'>
+              33
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box className={classes.box}>
+            <Typography variant='h4'>Order Amount</Typography>
+            <Typography variant='h2' fontWeight='700'>
+              33
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
+
+export default AmountStatistic;
--- admin-dance/src/pages/Dashboard.tsx
+++ admin-dance/src/pages/Dashboard.tsx
@@ -3,19 +3,23 @@ import Dog from '../assets/svgs/Dog';
 import { Box } from '@mui/system';
 import { Container, Grid, Stack, Typography } from '@mui/material';
 import Toolkit from '../components/dashboard/Toolkit';
-
+import AmountStatistic from '../components/dashboard/AmountStatistic';
+import Chart from '../components/dashboard/Chart';
 
 const Dashboard = () => {
   return (
-    <Container style={{padding:'50px 20px'}} maxWidth={false} >
-     <Grid container direction='column' spacing={5}>
-       <Grid item>
-         <Toolkit />
-       </Grid>
-       <Grid item>
-         there is the chart
-       </Grid>
-     </Grid>
+    <Container style={{ padding: '50px 20px' }} maxWidth={false}>
+      <Grid container direction='column' spacing={3}>
+        <Grid item>
+          <Toolkit />
+        </Grid>
+        <Grid item>
+          <AmountStatistic />
+        </Grid>
+        <Grid item>
+          <Chart />
+        </Grid>
+      </Grid>
     </Container>
   );
 };
