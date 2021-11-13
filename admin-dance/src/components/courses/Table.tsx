739e091601165ebf8f69727966b62279495d43e3
hireoochen@gmail.com <hireoochen@gmail.com>
2021-11-13 12:58:23 +0800

    rewrite the table layout structure

--- admin-dance/src/components/courses/Table.tsx
+++ admin-dance/src/components/courses/Table.tsx
@@ -1,4 +1,15 @@
-import { Box, Container, Table, TableBody, TableCell, TableHead, TableRow, TableFooter, TablePagination} from '@mui/material';
+import {
+  Box,
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableFooter,
+  TablePagination,
+  TableContainer,
+} from '@mui/material';
 import React from 'react';
 
 const CourseTable = () => {
@@ -13,39 +24,71 @@ const CourseTable = () => {
               'rgb(63 63 68 / 5%) 0px 0px 0px 1px, rgb(63 63 68 / 15%) 0px 1px 2px 0px',
             transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
             display: 'flex',
-            overflow: 'scroll'
+            overflow: 'scroll',
+            flexDirection: 'column',
           }}
         >
-          <Table aria-label='course table'>
-            <TableHead>
-              <TableRow>
-                <TableCell>Dessert (100g serving)</TableCell>
-                <TableCell align='right'>Calories</TableCell>
-                <TableCell align='right'>Fat&nbsp;(g)</TableCell>
-                <TableCell align='right'>Carbs&nbsp;(g)</TableCell>
-                <TableCell align='right'>Protein&nbsp;(g)</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-            <TableCell component="th" scope="row">
-                1
-              </TableCell>
-              <TableCell align="right">2</TableCell>
-              <TableCell align="right">3</TableCell>
-              <TableCell align="right">4</TableCell>
-              <TableCell align="right">5</TableCell>
-            </TableBody>
-            <TableFooter>
-                <TablePagination
-                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                colSpan={3}
-                count={5}
-                rowsPerPage={5}
-                page={5}
-                onPageChange={()=>{}}
-                />
-            </TableFooter>
-          </Table>
+          <TableContainer sx={{ maxHeight: 800 }}>
+            <Table stickyHeader aria-label='sticky table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell> dd</TableCell>
+                  <TableCell> mm</TableCell>
+                  <TableCell> dcc</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                  <TableCell>33</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component='div'
+            count={15}
+            rowsPerPage={20}
+            page={5}
+            onPageChange={() => {}}
+          />
         </Box>
       </Container>
     </>
