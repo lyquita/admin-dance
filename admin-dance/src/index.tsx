9aecbc87eff018679413cc8a6fbb233ec2155506
hireoochen@gmail.com <hireoochen@gmail.com>
2021-11-16 10:43:40 +0800

    submit login form

--- admin-dance/src/index.tsx
+++ admin-dance/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api-dance.hireoo.fun';
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 ReactDOM.render(
     <BrowserRouter>
--- admin-dance/src/pages/Login.tsx
+++ admin-dance/src/pages/Login.tsx
@@ -7,12 +7,47 @@ import {
   Grid,
   Typography,
   TextField,
-  Button
+  Button,
 } from '@mui/material';
 
 import { User } from '../interfaces/Login';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
 
-const Login = () => {
+const Login: React.FC = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState<string>('hireoo');
+  const [password, setPassword] = useState<string>('');
+  const loginForm: User = {
+    username: username,
+    password: password,
+  };
+
+  function handleUsername(e: React.FormEvent) {
+    return setUsername((e.target as HTMLInputElement).value);
+  }
+
+  function handlePassword(e: React.FormEvent) {
+    return setPassword((e.target as HTMLInputElement).value);
+  }
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    axios
+      .post('/token/', loginForm)
+      .then(
+        (res) => {
+          localStorage.setItem('access_token', res.data.access),
+          localStorage.setItem('refresh_token', res.data.refresh),
+          navigate('/', {replace:true});
+        }
+      )
+      .catch((err)=> {
+        console.log('error', err);
+      });
+  }
 
   return (
     <>
@@ -26,23 +61,27 @@ const Login = () => {
           alignItems: 'center',
         }}
       >
-        <Container maxWidth="md">
+        <Container maxWidth='md'>
           <Grid
             container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
+            direction='column'
+            justifyContent='center'
+            alignItems='center'
             spacing={4}
           >
             <Grid item>
-              <Typography variant="h1">You look so nice today:)</Typography>
+              <Typography variant='h1'>You look so nice today:)</Typography>
             </Grid>
             <Grid item>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <Stack spacing={2}>
-                  <TextField label="username" />
-                  <TextField label="password" type="password" />
-                  <Button>Login</Button>
+                  <TextField label='username' onChange={handleUsername} />
+                  <TextField
+                    label='password'
+                    type='password'
+                    onChange={handlePassword}
+                  />
+                  <Button type='submit'>Login</Button>
                 </Stack>
               </form>
             </Grid>
