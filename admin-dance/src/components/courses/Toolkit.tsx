e9a54832fc0fc812f982f79967d2650fd3224ff3
hireoochen@gmail.com <hireoochen@gmail.com>
2021-11-24 21:41:35 +0800

    search field options

--- admin-dance/src/components/courses/Toolkit.tsx
+++ admin-dance/src/components/courses/Toolkit.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Autocomplete,
   Box,
@@ -10,8 +10,36 @@ import {
 import { DatePicker } from '@mui/lab';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import axios from 'axios';
+
 
 const Toolkit = () => {
+
+const [coachNameOptions, setCoachNameOptions] = useState([]);
+const [placeNameOptions, setPlaceNameOptions] = useState([]);
+const [courseNameOptions, setCourseNameOptions] = useState([]);
+
+const handleOpenField = (e: React.SyntheticEvent) =>{
+  console.log('???', (e.target as HTMLInputElement).id);
+};
+
+useEffect(()=>{
+  axios('course/coachname')
+  .then( res => setCoachNameOptions(res.data) )
+  .catch( err => Promise.reject(err));
+
+  axios('course/placename')
+  .then( res => setPlaceNameOptions(res.data))
+  .catch( err => Promise.reject(err));
+
+  axios('course/coursename')
+  .then( res => setCourseNameOptions(res.data))
+  .catch( err => Promise.reject(err));
+
+}, []);
+  
+
+
   const options = [
     { title: 'The Shawshank Redemption', year: 1994 },
     { title: 'The Godfather', year: 1972 },
@@ -36,9 +64,11 @@ const Toolkit = () => {
           <Grid container spacing={2} direction={{ xs: 'column', sm: 'row' }}>
             <Grid item>
               <Autocomplete
+                id='coachname'
                 sx={{ width: { md: 300, sm: 200 } }}
-                options={options.map((option) => option.title)}
+                options={coachNameOptions.map((option) => option.coachname)}
                 freeSolo
+                onOpen={handleOpenField}
                 renderInput={(params) => (
                   <TextField {...params} label='Select Coach' />
                 )}
@@ -46,8 +76,9 @@ const Toolkit = () => {
             </Grid>
             <Grid item>
               <Autocomplete
+                id='placename'
                 sx={{ width: { md: 300, sm: 200 } }}
-                options={options.map((option) => option.title)}
+                options={placeNameOptions.map((option) => option.placename)}
                 freeSolo
                 renderInput={(params) => (
                   <TextField {...params} label='Select PlaceName' />
@@ -56,8 +87,9 @@ const Toolkit = () => {
             </Grid>
             <Grid item>
               <Autocomplete
+                id='coursename'
                 sx={{ width: { md: 300, sm: 200 } }}
-                options={options.map((option) => option.title)}
+                options={courseNameOptions.map((option) => option.coursename)}
                 freeSolo
                 renderInput={(params) => (
                   <TextField {...params} label='Select CourseType' />
